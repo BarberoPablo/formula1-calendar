126621447dcb401800787537feac66fb95115449
Pablo Barbero <pablobarbero220@gmail.com>
2024-09-22 00:15:08 -0300

    fix: nextjs error

--- src/app/components/standings/StandingsTable.tsx
+++ src/app/components/standings/StandingsTable.tsx
@@ -1,10 +1,10 @@
 "use client";
 
+import { teamColors } from "@/app/lib/constants";
 import { drivers, teams } from "@/app/lib/mockData";
-import type { Driver } from "@/app/lib/types";
+import type { Driver, Team } from "@/app/lib/types";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
-//import { teamColors } from "../lib/constants";
 
 export default function StandingsTable() {
   const [showDrivers, setShowDrivers] = useState(true);
@@ -26,25 +26,31 @@ export default function StandingsTable() {
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -20 }}
           transition={{ duration: 0.3 }}
-          className="flex flex-col items-end md:flex-row"
+          className="flex flex-col justify-center md:flex-row"
         >
           <div className="overflow-x-auto mb-8">
             <table className="min-w-full bg-white">
               <thead>
-                <tr className="bg-gray-200 uppercase text-sm leading-normal">
-                  <th className="py-3 px-6 text-left">Position</th>
+                <tr className="bg-gray-200 uppercase text-sm border-l-4 border-gray-200">
+                  <th className="py-3 px-6 text-left">Pos</th>
                   <th className="py-3 px-6 text-left">{showDrivers ? "Driver" : "Team"}</th>
                   {showDrivers && <th className="py-3 px-6 text-left">Team</th>}
                   <th className="py-3 px-6 text-left">Points</th>
                 </tr>
               </thead>
-              <tbody className="text-sm font-light">
+              <tbody className="text-sm">
                 {(showDrivers ? drivers : teams).map((item, index) => (
-                  <tr key={item.id} className={`border-b border-gray-200 hover:bg-gray-100 ${index % 2 && "bg-gray-50"}`}>
-                    <td className="py-3 px-6 text-left whitespace-nowrap">{index + 1}</td>
-                    <td className="py-3 px-6 text-left">{item.name}</td>
-                    {showDrivers && <td className="py-3 px-6 text-left">{(item as Driver).team}</td>}
-                    <td className="py-3 px-6 text-left">{item.points}</td>
+                  <tr
+                    key={item.id}
+                    className={`border-b border-gray-200 hover:bg-gray-100 ${index % 2 && "bg-gray-50"} border-l-4`}
+                    style={{
+                      borderLeftColor: showDrivers ? teamColors[(item as Driver).team][0] : teamColors[(item as Team).name][0],
+                    }}
+                  >
+                    <td className="h-full py-3 px-6 flex justify-center">{index + 1}</td>
+                    <td className="h-full py-3 px-6 text-left">{item.name}</td>
+                    {showDrivers && <td className="h-full py-3 px-6 text-left">{(item as Driver).team}</td>}
+                    <td className="h-full py-3 px-6 text-left">{item.points}</td>
                   </tr>
                 ))}
               </tbody>
